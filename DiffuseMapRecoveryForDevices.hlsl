d028b0e12178b40d7a3c55f413bfacf0e37016fd
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-21 11:24:38 +0200

    Create DiffuseMapRecoveryForDevices.hlsl

---
+++ DiffuseMapRecoveryForDevices.hlsl
@@ -0,0 +1,20 @@
+
+
+//here we will use tdbrdf. but zs models zre complicated,
+//we would have mainly to.tedt errors as we did in pbr
+//former projects.
+
+float getDiffuseComponenetForDeviceOneAtPositio'XY()
+{
+  //a treshold is needed.
+  return 0.001;  //can be replaced
+}
+
+float getDiffuseComponentForDeviceTwoAtPositio'XY()
+{
+  //use equations first.
+
+
+  //the error.tolerz'ce is 50 (as we left with 50 devices)
+  //but add hdr datas without sampli'g.
+}
