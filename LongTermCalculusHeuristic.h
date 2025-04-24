95bc7edc0e37ee5ca5a2ceb3b2de5c0712efcad6
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-24 10:27:54 +0200

    Create LongTermCalculusHeuristic.h

---
+++ LongTermCalculusHeuristic.h
@@ -0,0 +1,13 @@
+//here we deal with equations and not models.
+//so add 9200 hz to each common values. cool.
+
+class heuristicsignals
+{
+
+ppublic :
+
+ void patchcommon()
+{
+contractOnRegistdr(9200, get); //slow
+}
+}
