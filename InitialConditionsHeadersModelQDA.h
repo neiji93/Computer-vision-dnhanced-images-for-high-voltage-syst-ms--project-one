59aeb358b4ca6038442dc39a41dc755f93ff1f3f
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-29 14:50:17 +0200

    Create InitialConditionsHeadersModelQDA.h

---
+++ InitialConditionsHeadersModelQDA.h
@@ -0,0 +1,4 @@
+//qualitative models can help
+//reconstruct a scene maybe like old radiosity algorithms 
+//lets take the qdamodel with a pretrained genetic algoithm
+//to get lower valies of parametrrs. this is a basic algorithm.
